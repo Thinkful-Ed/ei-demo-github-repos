4f0ea20270eab03456ad7a3f47529cc124dbe5b5
Joe Turner <joe@oampo.co.uk>
2018-02-21 13:11:04 +0000

    Add sync action

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,13 @@
-export const fetchRepos = username => () => {
+export const FETCH_REPOS_SUCCESS = 'FETCH_REPOS_SUCCESS';
+export const fetchReposSuccess = repos => ({
+    type: FETCH_REPOS_SUCCESS,
+    repos
+});
+
+export const fetchRepos = username => dispatch => {
     return fetch(`https://api.github.com/users/${username}/repos`)
         .then(res => res.json())
-        .then(repos => console.log(repos.map(repo => repo.name)));
+        .then(repos =>
+            dispatch(fetchReposSuccess(repos.map(repo => repo.name)))
+        );
 };
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,14 @@
+import {FETCH_REPOS_SUCCESS} from './actions';
+
 const initialState = {
     repos: ['foo', 'bar', 'baz']
 };
 
 export default (state = initialState, action) => {
+    if (action.type === FETCH_REPOS_SUCCESS) {
+        return Object.assign({}, state, {
+            repos: action.repos
+        });
+    }
     return state;
 };
